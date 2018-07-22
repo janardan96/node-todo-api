6a8c50001a5d1b25b44ad03bfe8dd4c5f05a8666
janardan96 <jdcodemon@gmail.com>
2018-07-21 17:45:14 -0700

    Add delete todos/id

--- server/server.js
+++ server/server.js
@@ -15,6 +15,8 @@ var {
     User
 } = require("./models/user");
 
+const port = process.env.PORT || 3000;
+
 var app = express();
 app.use(bodyParser.json());
 
@@ -49,19 +51,36 @@ app.get("/todos/:id", (req, res) => {
     }
 
     Todo.findById(id).then((todo) => {
-        if(!todo){
+        if (!todo) {
             return res.status(404).send("Bad Request");
         }
 
-        res.status(200).send({todo});
-    }).catch((e)=>{
+        res.status(200).send({
+            todo
+        });
+    }).catch((e) => {
         res.status(400).send();
     });
 
+});
+
+app.delete("/todos/:id", (req, res) => {
+    var id = req.params.id;
+    if (!ObjectId.isValid(id)) {
+        res.status(404).send("NO item is deleted because id is not found");
+    }
+
+    Todo.findOneAndRemove(id).then((remove) => {
+        if (!remove) {
+            return res.status(404).send("Incorrect id");
+        }
+        res.status(200).send(remove);
+    }).catch((e) => res.status(400).send());
+
 })
 
-app.listen(3000, () => {
-    console.log("Server is started");
+app.listen(port, () => {
+    console.log(`Started up at port ${port}`);
 });
 
 module.exports = {
